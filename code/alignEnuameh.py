88444a5be05f2e77b75289ed0175f41128a163be
Josh Wetzel <jlwetzel@princeton.edu>
2022-05-31 20:18:31 -0400

    testing based on b1h data

---
+++ code/alignEnuameh.py
@@ -0,0 +1,54 @@
+# A script for determining the correct orientation of the
+# full-length Enuameh PWMs by finding the optimal alignment of 
+# each to its single-finger concatenated version going in reverse-finger order
+
+from matAlignLib import comp_matrices, matrix_compl, information_content
+from gibbsAlign_GLM import getPrecomputedInputs_zfC2H2
+
+def getPWMsRevFingOrder(fname):
+    fin = open(fname, 'r')
+    line = fin.readline()
+    line = fin.readline()
+    currentProt = ''
+    pwms = {}
+    i = 0
+    while line != '':
+        #print i
+        l = line.strip().split('\t')
+        lastProt = currentProt
+        currentProt = l[0]
+        if currentProt != lastProt:
+            if i != 0:
+                pwms[lastProt] = pwm
+            pwm = []
+        pwm.append([float(x) for x in l[4:]])
+        line = fin.readline()
+        i += 1
+    pwms[lastProt] = pwm
+    fin.close()
+
+    return pwms
+
+def main():
+    pwms_full, _, _, _, _ = \
+        getPrecomputedInputs_zfC2H2(rescalePWMs = False, ffsOnly = True, includeB1H = False)
+    pwms_revFingOrder = getPWMsRevFingOrder('../flyFactorSurvey/enuameh/enuameh_perFinger_PWMs_reverseFingerOrder.txt')
+    fout = open('../flyFactorSurvey/enuameh/enuameh_startPosInfo.txt', 'w')
+    fout.write('prot\tstart\trev\n')
+    for p in sorted(pwms_full.keys()):
+        #print len(pwms_full[p]), len(pwms_revFingOrder[p])
+        if len(pwms_revFingOrder[p]) > len(pwms_full[p]):
+            continue
+        pWid = len(pwms_full[p])
+        
+        # Align the annotated finger PWM to the 
+        score, shift, ori = comp_matrices(pwms_full[p], pwms_revFingOrder[p],
+                                          minWidthM2 = len(pwms_revFingOrder[p]), 
+                                          oneSided=True)
+        #print p, shift, int(ori)
+        fout.write('%s\t%d\t%d\n' %(p,shift,int(ori)))
+    fout.close()
+
+
+if __name__ == '__main__':
+    main()
