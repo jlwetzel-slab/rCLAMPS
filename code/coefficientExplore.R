bb26cfbc48aca892d1fa5fd9b62814f4c7f36b7d
jlwetzel-slab <jlwetzel@princeton.edu>
2022-07-15 20:57:41 -0400

    added coefficientExplore.R

---
+++ code/coefficientExplore.R
@@ -0,0 +1,25 @@
+# A script for basic exploration of coefficients of learned by rCLAMPS
+
+library(data.table)
+library(ggplot2)
+library(RColorBrewer)
+rm(list = ls())
+
+DOMAIN_TYPE = 'zf-C2H2'
+
+x <- fread(paste0('../examplePredictions/',DOMAIN_TYPE,'/coefTable.txt'))
+
+# Create a 4 X 19 grid of coefficients and plot relative to 0
+ref.base = 'T'; bp <- 2; ap <- 3
+contacts <- x[bpos == bp & aapos == ap]
+#contacts$coef <- sapply(1:nrow(contacts), function(i) {
+#  ref.delta <- contacts[base == ref.base & aa == contacts$aa[i]]$coef
+#  contacts$coef[i] - ref.delta
+#})
+#contacts <- contacts[base != ref.base]
+
+rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
+r <- rf(32)
+g <- ggplot(droplevels(contacts), aes(x = aa, y = base, fill = coef)) + 
+  scale_fill_gradient2() +
+  geom_tile(color = 'black')
