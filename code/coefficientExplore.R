2e04e19a2205415824305e3115c9e3a9950cc669
jlwetzel-slab <jlwetzel@princeton.edu>
2022-07-15 23:36:35 -0400

    testing coefficientExplore.R

--- code/coefficientExplore.R
+++ code/coefficientExplore.R
@@ -6,24 +6,30 @@ library(RColorBrewer)
 library(scales)
 rm(list = ls())
 
-DOMAIN_TYPE = 'zf-C2H2'
+DOMAIN_TYPE <- 'homeodomain'  #Set to 'homeodomain' or 'zf-C2H2'
+PLOT_TYPE <- 'coef' # set to 'coef' or 'offset'
+PLOT_DIR <- paste0('../examplePredictions/',DOMAIN_TYPE,'/',PLOT_TYPE,'_plots/')
+dir.create(PLOT_DIR, showWarnings = FALSE, recursive = TRUE)
 
 x <- fread(paste0('../examplePredictions/',DOMAIN_TYPE,'/coefTable.txt'))
-#x <- x[complete.cases(x)]
 contacts.all <- x[complete.cases(x)]
 intercepts <- x[is.na(aapos)]
+intercepts$intercept <- intercepts$coef
+intercepts <- intercepts[,c('bpos', 'base', 'intercept'),with=FALSE]
+contacts.all <- merge(contacts.all, intercepts, by = c('bpos','base'))
+contacts.all$offset <- contacts.all$coef + contacts.all$intercept
+contacts.all$contactPair <- paste(paste0('b',contacts.all$bpos), 
+                                  paste0('a',contacts.all$aapos), sep = '.')
+contacts.all$contactPair <- factor(contacts.all$contactPair)
 
-# Create a 4 X 19 grid of coefficients and plot relative to 0
-ref.base = 'T'; bp <- 1; ap <- 6
-contacts <- x[bpos == bp & aapos == ap]
-contacts$coef <- sapply(1:nrow(contacts), function(i) {
-  ref.delta <- contacts[base == ref.base & aa == contacts$aa[i]]$coef
-  contacts$coef[i] - ref.delta
-})
-contacts <- contacts[base != ref.base]
-
-rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
-r <- rf(32)
-g <- ggplot(droplevels(contacts), aes(x = aa, y = base, fill = coef)) + 
-  scale_fill_gradient2(low = muted("blue"), high = muted("red")) +
-  geom_tile(color = 'black')
+for (cp in levels(contacts.all$contactPair)) {
+  contacts <- contacts.all[contactPair == cp]
+  
+  # Create a 4 X 19 grid of coefficients and plot of relative offsets
+  rf <- colorRampPalette(rev(brewer.pal(11,'Spectral')))
+  r <- rf(32)
+  g <- ggplot(droplevels(contacts), aes_string(x = 'aa', y = 'base', fill = PLOT_TYPE)) + 
+    scale_fill_gradient2(low = muted("blue"), high = muted("red")) +
+    geom_tile(color = 'black')
+  ggsave(g, file=paste0(PLOT_DIR,PLOT_TYPE,'_',cp,'.pdf'), width = 8, height = 2)
+}
